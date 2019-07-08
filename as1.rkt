fe6408d877868b6805caa3abb9c6a7c5a42c0f98
ANAMI Taichi <daylight87m_p@yahoo.co.jp>
2019-07-08 14:17:25 +0900

    課題1その1が完了

---
+++ as1.rkt
@@ -0,0 +1,22 @@
+(define tree1 '(1 (2 (3 4)) 6 (7 8 9)))
+(define mymap
+  (lambda (fn ls)
+    (if (null? ls)
+        '()
+        (cons (fn (car ls))(mymap fn (cdr ls)))
+        )
+    )
+  )
+
+(define cb (lambda(x)(* x x)))
+
+(define map-tree
+  (lambda(fn tree)
+    (cond ((null? tree) '())
+          ( (pair? tree)
+            (cons (map-tree fn (car tree))(map-tree fn (cdr tree)))
+            )
+          (else (fn tree))
+          )
+    )
+  )
