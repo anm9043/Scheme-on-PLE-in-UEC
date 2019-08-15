d594997a15fc8a6a8871a88cd9bbb0f84c425ad1
ANAMI Taichi <daylight87m_p@yahoo.co.jp>
2019-08-15 16:35:12 +0900

    関数sons, subtreeを追加

--- as2.rkt
+++ as2.rkt
@@ -20,6 +20,17 @@
 (define kakeizu
   (read (open-input-file "kakeizu")))
 
+(define (sons tree) ;treeのトップの子要素のリストを返す
+  (map car (cdr tree)))
+
+(define (subtree num tree) ;treeのnum番目のサブツリーを返す
+  (if (> num (length (cdr tree))) ;treeの子要素数がnumよりも少ない場合は
+      #f                          ;#fを返す
+      (let loop ((i num)(t tree))
+        (if (< 1 i)
+            (loop (- i 1)(cdr t))
+            (car (cdr t))))))
+
 (define get-depth
   (lambda (famtre depth)
     (cond ((or (null? famtre) (< depth 1)) '())
